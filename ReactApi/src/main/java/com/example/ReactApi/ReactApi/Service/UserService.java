87a320a74a92081c625cec27ddc971492b110467
Vaijnath Magar <90306483+VaijnathMagar@users.noreply.github.com>
2024-01-23 08:35:59 +0530

    Add files via upload

---
+++ ReactApi/src/main/java/com/example/ReactApi/ReactApi/Service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.ReactApi.ReactApi.Service;
+
+	import net.javaguides.springboot.dto.UserDto;
+	import net.javaguides.springboot.entity.User;
+
+	import java.util.List;
+
+	public interface UserService {
+	    void saveUser(UserDto userDto);
+
+	    User findUserByEmail(String email);
+
+	    List<UserDto> findAllUsers();
+	}
---
+++ ReactApi/src/test/java/com/example/ReactApi/ReactApi/ReactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReactApi.ReactApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
